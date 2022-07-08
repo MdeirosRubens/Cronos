14d43a8954c11c87f22adc8d763f6b57125e2c25
MdeirosRubens <mdeirosrubens@outlook.com>
2022-07-08 12:34:14 -0300

    Desenvolvendo a lista de notificações

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,17 @@
+import { INotificacao } from "./../interfaces/INotificacao";
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO} from "./tipo-mutacoes"
+import {
+  ADICIONA_PROJETO,
+  ALTERA_PROJETO,
+  EXCLUIR_PROJETO,
+  NOTIFICAR,
+} from "./tipo-mutacoes";
 
 interface Estado {
   projetos: IProjeto[];
+  notificacoes: INotificacao[];
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol();
@@ -12,6 +19,7 @@ export const key: InjectionKey<Store<Estado>> = Symbol();
 export const store = createStore<Estado>({
   state: {
     projetos: [],
+    notificacoes: [],
   },
   mutations: {
     [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
@@ -26,8 +34,16 @@ export const store = createStore<Estado>({
       state.projetos[index] = projeto;
     },
     [EXCLUIR_PROJETO](state, id: string) {
-      state.projetos = state.projetos.filter(proj => proj.id != id)
-    }
+      state.projetos = state.projetos.filter((proj) => proj.id != id);
+    },
+    [NOTIFICAR](state, novaNotificacao: INotificacao) {
+      novaNotificacao.id = new Date().getTime();
+      state.notificacoes.push(novaNotificacao);
+
+      setTimeout(() => {
+        state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
+      }, 3000);
+    },
   },
 });
 
